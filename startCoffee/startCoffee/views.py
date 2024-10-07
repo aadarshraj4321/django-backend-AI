96c90db6ea318f39821cb3bd21650ea03b2c1491
aadarshraj4321 <aadarshraj4321@gmail.com>
2024-10-08 01:22:28 +0530

    added template and static folder for HTML and CSS/JavaScript

--- startCoffee/startCoffee/views.py
+++ startCoffee/startCoffee/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+
+
+# def home(request):
+#     return HttpResponse("Hola World! Home Page")
+
+# def about(request):
+#     return HttpResponse("<h1>Hola this is About Page</h1> <h2>this is H2 tag</h2>")
 
 
 def home(request):
-    return HttpResponse("Hola World! Home Page")
+    return render(request, "website/index.html")
 
 
 
